f12395def77f03a13951acada8ad8a6c940437e7
Nabil Daoud <nabiljdaoud@gmail.com>
2018-02-21 19:23:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function add2(n) {
   return n + two;
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function outer() {
+  return function inner() {
     return "FUNKY!";
   };
 };
